fe3f8c193b88ae34d13ab8078f6ab1ead0abf485
darshna <darshna2302@gmail.com>
2026-04-06 12:18:24 +0530

    cors error fixed

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ connectDB();
 const app=express();
 const port=process.env.PORT || 4000;
 
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','http://localhost:3001'];
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','http://localhost:3001','https://task-auth-frontend.onrender.com'];
 console.log("Allowed Origins:", allowedOrigins);
 
  //all req parsed as json

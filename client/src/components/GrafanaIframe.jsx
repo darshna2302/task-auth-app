713319c1f08109af8b2eeba5e81506d1c7bc7a4c
darshna <darshna2302@gmail.com>
2026-04-09 10:22:21 +0530

    grafana iframe

--- client/src/components/GrafanaIframe.jsx
+++ client/src/components/GrafanaIframe.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function GrafanaIframe() {
   return (
-    <div>
+    <div className='p-4'>
         <h1>Grafana with Iframe</h1>
-        <iframe src="http://localhost:3000/d-solo/adckdth/new-dashboard?orgId=1&from=1775686737651&to=1775708337651&timezone=browser&panelId=panel-1&__feature.dashboardScene=true" width="450" height="200" ></iframe>
+        <iframe src="http://localhost:3003/d-solo/adckdth/new-dashboard?orgId=1&from=1775686737651&to=1775708337651&timezone=browser&panelId=panel-1&__feature.dashboardScene=true" width="950" height="400" ></iframe>
     </div>
   )
 }
